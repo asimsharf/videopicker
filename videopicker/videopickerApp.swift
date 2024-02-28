2354507ef9c7d3694f2b64816d1e11515210a23c
asimsharf <asimsahrf@gmail.com>
2024-02-28 21:31:16 +0300

    Initial Commit

---
+++ videopicker/videopickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  videopickerApp.swift
+//  videopicker
+//
+//  Created by asimsharf on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct videopickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ videopickerUITests/videopickerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  videopickerUITestsLaunchTests.swift
+//  videopickerUITests
+//
+//  Created by asimsharf on 28/02/2024.
+//
+
+import XCTest
+
+final class videopickerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
